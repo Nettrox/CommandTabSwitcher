3696bfc07276459dfc2000d553069b8d164e3d0f
İrfan Batuhan Aksu <nettroxaksu@gmail.com>
2026-07-16 15:15:44 +0300

    Initial Commit

---
+++ CommandTabSwitcher/CommandTabSwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommandTabSwitcherApp.swift
+//  CommandTabSwitcher
+//
+//  Created by Irfan Aksu on 16.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct CommandTabSwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CommandTabSwitcherTests/CommandTabSwitcherTests.swift
@@ -0,0 +1,17 @@
+//
+//  CommandTabSwitcherTests.swift
+//  CommandTabSwitcherTests
+//
+//  Created by Irfan Aksu on 16.07.2026.
+//
+
+import Testing
+@testable import CommandTabSwitcher
+
+struct CommandTabSwitcherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
